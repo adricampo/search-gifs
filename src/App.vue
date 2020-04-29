eeac4171f3b8605cd2c18971ebac6ada104aa122
Adrian Campo Calvo <Adri@MacBook-Pro-de-Adrian.local>
2020-04-29 20:01:11 +0200

    last modifications

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <div id="app" class="app" @showGifs="gifs">
+  <div id="app" class="app">
     <Header />
     <img width="250px" alt="Vue logo" src="./assets/gif-logo.gif" />
     <SearchForm @retrieveGifs="gifsList" />
-    <BestGifs @retrieveTopGifs1="gifsList" @retrieveTopGifs2="gifsList" @retrieveTopGifs3="gifsList" />
+    <BestGifs
+      @retrieveTopGifs1="gifsList"
+      @retrieveTopGifs2="gifsList"
+      @retrieveTopGifs3="gifsList"
+    />
     <Results :gifs="gifs" />
   </div>
 </template>
@@ -11,7 +15,7 @@
 <script>
 import Header from "./components/Header/Header";
 import SearchForm from "./components/SearchForm/SearchForm";
-import Results from "./components/Results/Results"
+import Results from "./components/Results/Results";
 import BestGifs from "./components/BestGifs/BestGifs";
 
 export default {
@@ -22,32 +26,29 @@ export default {
     Results,
     BestGifs
   },
-  data () {
+  data() {
     return {
       gifs: []
-    }
+    };
   },
   methods: {
-    //call to API to get data
+    // API call to get data using a query
     async gifsList(queryInput) {
       try {
         const response = await fetch(
           `https://api.giphy.com/v1/gifs/search?api_key=n3bZe4akFHmonoAyeLs5KLeeHGwU2xw7&q=${queryInput}&limit=25&offset=0&rating=G&lang=en`
         );
         const result = await response.json();
-        const gifs = result.data
+        const gifs = result.data;
 
         this.gifs = gifs;
-        this.$emit('showGifs', gifs)
+        this.$emit("showGifs", gifs);
       } catch (error) {
         throw new Error("something went wrong..");
       }
-    },
-  
-  },
-
-}
-  
+    }
+  }
+};
 </script>
 
 <style lang="sass">
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import BestGifs from './components/BestGifs/BestGifs.vue'
 import Header from './components/Header/Header.vue'
 import SearchForm from './components/SearchForm/SearchForm.vue'
+import Results from './components/Results/Results.vue'
 
 Vue.config.productionTip = false
 
@@ -13,8 +14,7 @@ const router = new VueRouter({
   mode: 'history',
   routes: [{
     path: '/',
-    name: 'gif',
-    components: [App, BestGifs, Header, SearchForm],
+    components: [App, BestGifs, Header, SearchForm, Results],
     props: true
   }]
 })
